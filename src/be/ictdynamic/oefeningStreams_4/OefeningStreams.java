53fe3a0e8d7f970b25ce9a79741e43daada81519
wvdbrande <wim.vandenbrande@one-agency.be>
2017-09-19 14:59:49 +0200

    Added SortedSet

--- src/be/ictdynamic/oefeningStreams_4/OefeningStreams.java
+++ src/be/ictdynamic/oefeningStreams_4/OefeningStreams.java
@@ -17,7 +17,7 @@ public class OefeningStreams {
     private Set<Employee> employees;
 
     public OefeningStreams() {
-        employees = this.getEmployees();
+        employees = this.initEmployees();
 
         Employee employee = new Employee(1, "wim van den brande", 50, Worker.Gender.MALE, null);
         Set<Department> departments1 = new LinkedHashSet<>();
@@ -46,7 +46,7 @@ public class OefeningStreams {
         employees.add(employee);
     }
 
-    public Set<Employee> getEmployees() {
+    public Set<Employee> initEmployees() {
         if (employees == null) {
             employees = new LinkedHashSet<>(10);
         }
@@ -59,33 +59,33 @@ public class OefeningStreams {
 
     public void exec() {
         // possible NPE !!!
-        List<Employee> vdbEmployees1 = getEmployees().stream().filter(employee -> employee.getName().toLowerCase().lastIndexOf("van den brande") >= 0).collect(Collectors.toList());
+        List<Employee> vdbEmployees1 = initEmployees().stream().filter(employee -> employee.getName().toLowerCase().lastIndexOf("van den brande") >= 0).collect(Collectors.toList());
         System.out.println("number of employees = " + vdbEmployees1.size() + ", : " + vdbEmployees1);
-        List<Employee> vdbEmployees2 = getEmployees().stream().filter((employee) -> employee.getName() != null && employee.getName().toLowerCase().lastIndexOf("van den brande") >= 0).collect(Collectors.toList());
+        List<Employee> vdbEmployees2 = initEmployees().stream().filter((employee) -> employee.getName() != null && employee.getName().toLowerCase().lastIndexOf("van den brande") >= 0).collect(Collectors.toList());
         System.out.println("number of employees = " + vdbEmployees2.size() + ", : " + vdbEmployees2);
 
         // filter, sorted, and map, which can be connected together to form a pipeline
         // collect closes the pipeline and returns a result
 
-        List<Employee> femaleEmployees = getEmployees().stream().filter(employee -> employee.getGender() == Worker.Gender.FEMALE).collect(Collectors.toList());
+        List<Employee> femaleEmployees = initEmployees().stream().filter(employee -> employee.getGender() == Worker.Gender.FEMALE).collect(Collectors.toList());
         System.out.println("number of female employees = " + femaleEmployees.size() + ", : " + femaleEmployees);
 
-        List<Integer> ages = getEmployees().stream().map(employee -> employee.getAge()).collect(Collectors.toList());
+        List<Integer> ages = initEmployees().stream().map(employee -> employee.getAge()).collect(Collectors.toList());
         System.out.println("number of ages = " + femaleEmployees.size() + ", : " + ages);
 
         // DO NOT check for nulls
 
         // Avoid coding a method that returns null
         // The idea is to not force calling code to immediately handle issues
-        List<Employee> otherEmployees = getEmployees().stream().filter(employee -> employee.getGender() == Worker.Gender.OTHER).collect(Collectors.toList());
+        List<Employee> otherEmployees = initEmployees().stream().filter(employee -> employee.getGender() == Worker.Gender.OTHER).collect(Collectors.toList());
         System.out.println("number of other employees = " + otherEmployees.size() + ", : " + otherEmployees);
 
         // be aware - sorting is expensive
-        List<Employee> employeesSortedByAge = getEmployees().stream().sorted(comparing(Employee::getAge)).collect(Collectors.toList());
+        List<Employee> employeesSortedByAge = initEmployees().stream().sorted(comparing(Employee::getAge)).collect(Collectors.toList());
         System.out.println("employeesSortedByAge: " + employeesSortedByAge);
 
         // example of Optional
-        Optional<Employee> optionalEmployee = getEmployees().stream().filter(employee -> employee.getGender() == Worker.Gender.FEMALE).findFirst();
+        Optional<Employee> optionalEmployee = initEmployees().stream().filter(employee -> employee.getGender() == Worker.Gender.FEMALE).findFirst();
         if (optionalEmployee.isPresent()) {
             System.out.println("Our employees contain multiple genders.");
         }
@@ -97,23 +97,23 @@ public class OefeningStreams {
         // Parallel streams make sense if the collection is large enough and your computer has enough cores!!!
         // Issues ????
 
-        getEmployees().parallelStream().filter(employee -> employee.getGender() == Worker.Gender.OTHER).collect(Collectors.toList());
+        initEmployees().parallelStream().filter(employee -> employee.getGender() == Worker.Gender.OTHER).collect(Collectors.toList());
 
         // BTW : streams work on Arrays as well
         Employee myEmployeesArray[] = employeesSortedByAge.toArray(new Employee[employeesSortedByAge.size()]);
         List<Employee> employeesSortedByName = Arrays.stream(myEmployeesArray).sorted(comparing(Employee::getName)).collect(Collectors.toList());
         System.out.println("employeesSortedByName: " + employeesSortedByName);
 
-        Boolean anyMatchBoolean = getEmployees().stream().anyMatch(employee -> new Integer(71).equals(employee.getAge()));
+        Boolean anyMatchBoolean = initEmployees().stream().anyMatch(employee -> new Integer(71).equals(employee.getAge()));
         System.out.println("anyMatchBoolean: " + anyMatchBoolean);
 
-        Boolean allMatch = getEmployees().stream().allMatch(employee -> employee.getId() > 0);
+        Boolean allMatch = initEmployees().stream().allMatch(employee -> employee.getId() > 0);
         System.out.println("allMatchBoolean: " + allMatch);
 
         // example of the flatmap !!! (asked in an interview)
         // NPE als getDepartment geen lazy initialization bevat
         List<Department> departments =
-                getEmployees()
+                initEmployees()
                         .stream()
                         .map(Employee::getDepartment)
                         .flatMap(Collection::stream)
@@ -124,10 +124,49 @@ public class OefeningStreams {
 
         // streams and sort by Id (using Comparable interface and compareTo method)
         System.out.println("Streams and sort - example 1");
-        getEmployees().stream().sorted().forEach(System.out::println);
+        initEmployees().stream().sorted().forEach(System.out::println);
 
         // streams and sort by Age (using Comparable interface and compareTo method)
         System.out.println("Streams and sort - example 2");
-        getEmployees().stream().sorted(comparing(Worker::getAge)).forEach(System.out::println);
+        initEmployees().stream().sorted(comparing(Worker::getAge)).forEach(System.out::println);
+
+        // example of Sorted Set
+        // ---------------------
+
+        SortedSet<Employee> employeesSorted = new TreeSet<>();
+        Employee employee = new Employee(1, "wim van den brande", 50, Worker.Gender.MALE, null);
+        employeesSorted.add(employee);
+
+        Employee employee2 = new Employee(2, "kris van den brande", 48, null, null);
+        employeesSorted.add(employee2);
+
+        employee = new Employee(6, "michelle obama", 55, Worker.Gender.FEMALE, null);
+        employeesSorted.add(employee);
+
+        employee = new Employee(5, "donald trump", 71, Worker.Gender.MALE, null);
+        employeesSorted.add(employee);
+
+        Employee employee4 = new Employee(4, "hilary clinton", 72, Worker.Gender.FEMALE, null);
+        employeesSorted.add(employee4);
+
+        employee = new Employee(0 ,"jan van den brande", 52, Worker.Gender.MALE, null);
+        employeesSorted.add(employee);
+
+        System.out.println("employeesSortedDefault first: " + employeesSorted.first());
+        System.out.println("employeesSortedDefault last: " + employeesSorted.last());
+        // comparator will be null as we are using the Comparable
+        System.out.println("employeesSortedDefault: " + employeesSorted.comparator());
+
+        // usage of headset
+        // Returns a view of the portion of this set whose elements are strictly less than element
+        SortedSet headSet = employeesSorted.headSet(employee2);
+        System.out.println("headSet: " + headSet);
+
+        // usage of tailSet
+        // Returns a view of the portion of this set whose elements are greater than or equal to fromElement.
+        SortedSet tailSet = employeesSorted.tailSet(employee4);
+        System.out.println("tailSet: " + tailSet);
+
+
     }
 }
